d827390652b2d009be7bf3dde4e6289602b15f85
Trung DG <thanhtrung.it25@gmail.com>
2021-05-12 20:45:35 +0700

    Setup webpack client, server, hot reloader

---
+++ webpack.config.server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const CURRENT_WORKING_DIR = process.cwd();
+
+const config = {
+  name: 'server',
+  entry: [path.join(CURRENT_WORKING_DIR, './server/server.js')],
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  output: {
+    path: path.join(CURRENT_WORKING_DIR, '/dist/'),
+    filename: 'server.generated.js',
+    publicPath: '/dist/',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = config;
